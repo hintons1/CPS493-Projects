efaa2f4891d7f21543b1669c0279381ef62c9f5b
hintons1 <hintons1@newpaltz.edu>
2023-10-22 23:35:47 -0400

    fixing folders and file paths

---
+++ vue-project/src/views/UsersView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <article class="message is-danger">
+  <div class="message-header">
+    <p>Invalid Credentials</p>
+    <button class="delete" aria-label="delete"></button>
+  </div>
+  <div class="message-body">
+    Admin must sign in to modify any existing users.
+  </div>
+</article>
+</template>
