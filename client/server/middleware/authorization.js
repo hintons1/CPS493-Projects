d9c885ab93d300ea110b3ba04c1e98f7afe7fbb9
hintons1 <hintons1@newpaltz.edu>
2023-12-07 08:25:33 -0500

    completed authorization

--- client/server/middleware/authorization.js
+++ client/server/middleware/authorization.js
@@ -4,27 +4,30 @@ module.exports = {
     async parseAuthorization(req, res, next){
     const token = req.headers.authorization?.split(' ')[1];
     if(token){
-        users.verifyJWT(token);
-    }else{
+        const payload = await users.verifyJWT(token);
+        req.user = payload;
         next();
+    }else{
+        return next();
     }
-    }
-
+}
 },
 
-requireLogin(needAdmin = false){
+requireLogin(needAdmin) {
+    needAdmin = needAdmin || false;
     return (req, res, next) => {
         if(!req.user){
             return next({
                 status: 401,
                 message: 'Login Required'
-            })
+            });
         }
         if(needAdmin && !req.user.admin){
             return next({
                 status: 403,
                 message: 'Admin Required'
-            })
+            });
         }
-    }
+        next();
+    };
 }
